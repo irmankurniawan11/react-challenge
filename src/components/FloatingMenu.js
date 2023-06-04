6c3aa246db908146120725259a252bb8bafe487c
irmankurniawan11 <irmankurniawan99@gmail.com>
2023-06-04 22:00:51 +0700

    Update website metadata, data, and little styling

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -1,9 +1,18 @@
+import { useLocation } from "react-router-dom"
+
 const FloatingMenu = () => {
+
+    const location = useLocation();
+
+    const pathname = location.pathname.substring(location.pathname.lastIndexOf('/')+1);
+    const words = pathname.split('-');
+    const formattedText = words.map((word)=>word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
     return (
         <div className="rounded-none sm:rounded-xl fixed bottom-0 sm:bottom-4 z-[9999] lg:w-[50%] w-full left-[50%] -translate-x-1/2 bg-slate-800/80 px-8 py-5 text-white shadow-xl backdrop-blur-md">
             <div className="container mx-auto flex justify-between items-center">
                 <div>
-                    <h1 className="text-2xl font-bold">Holadok</h1>
+                    <h1 className="text-2xl font-bold">{formattedText}</h1>
                     <p className="text-sm">Challenge by Irman</p>
                 </div>
                 <a href="/" className="font-bold px-6 py-4 rounded-full bg-red-700 hover:bg-red-600">Back to Menu</a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import NavBar from "../components/NavBar";
-import { ArrowTopRightOnSquareIcon, ArrowRightIcon, ArrowSmallRightIcon } from '@heroicons/react/24/solid'
+import { ArrowTopRightOnSquareIcon, ArrowRightIcon, ArrowSmallRightIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const Home = () => {
     return (
@@ -14,21 +14,21 @@ const Home = () => {
 const Header = () => {
     return (
         <header className="container max-w-6xl mx-auto px-8 sm:px-0 mb-8 dark:text-white/90">
-            <h1 className="font-bold text-3xl sm:text-4xl my-4">Slicing UI Challenge by Irman</h1>
-            <p>Hasil slicing UI dari challenge desain dari <a className="text-amber-500" href="https://codedesign.dev/" target="_blank" rel="noreferrer" alt="codedesign">codedesign</a>.</p>
+            <h1 className="font-bold text-4xl sm:text-5xl my-4">Design to Code Challenge by Irman</h1>
+            <p>Hasil slicing UI "Design to Code Challenge" dari <a className="text-amber-500" href="https://codedesign.dev/" target="_blank" rel="noreferrer" alt="codedesign">codedesign</a>.</p>
         </header>
     )
 }
 
 const ChallengeCard = ( {imageUrl, title, desc, url, sourceUrl} ) => {
     return (
-        <div className="rounded-xl overflow-hidden shadow-md sm:w-64">
+        <div className="rounded-xl overflow-hidden shadow-md sm:w-72">
             <img src={imageUrl} className="w-full h-40 object-cover"/>
             <div className="p-4 bg-white dark:bg-slate-700">
                 <h1 className="font-bold text-lg mb-1">{title}</h1>
-                <p className="truncate text-sm">{desc}</p>
-                <div className="flex flex-wrap gap-2 mt-3">
-                    <a href={sourceUrl} className="font-bold flex items-center px-4 py-2 rounded-full text-sm ring-1 ring-slate-200 hover:bg-slate-100 dark:hover:bg-white/20 dark:ring-white/30">Source Code<ArrowTopRightOnSquareIcon className="w-4 h-4 ml-1"/></a>
+                <p className="text-sm line-clamp-2">{desc}</p>
+                <div className="flex justify-stretch gap-2 mt-3">
+                    <a href={sourceUrl} rel="noreferrer" target="_blank" className="font-bold flex items-center px-4 py-2 rounded-full text-sm ring-1 ring-slate-200 hover:bg-slate-100 dark:hover:bg-white/20 dark:ring-white/30">Source Code<ArrowTopRightOnSquareIcon className="w-4 h-4 ml-1"/></a>
                     <a href={url} className="font-bold flex items-center px-4 py-2 rounded-full text-sm ring-1 ring-blue-400 bg-blue-600 hover:bg-blue-500 text-white">Visit<ArrowSmallRightIcon className="w-4 h-4 ml-1"/></a>
                 </div>
             </div>
@@ -40,16 +40,16 @@ const Challenges = () => {
     const challengeData = [
         {
             title: "Twitter Embed",
-            desc: "Voluptate Lorem aliqua fugiat fugiat commodo.",
+            desc: "Twitter Embed is a simple card that displays a single tweet from Twitter. It includes the user's avatar, name, username, the tweet itself, the total number of likes, and the date it was published... - codedesign",
             imageUrl: "/image/preview-twitter-embed.png",
-            sourceUrl: "/challenge/twitter-embed",
+            sourceUrl: "https://github.com/irmankurniawan11/react-challenge/blob/main/src/pages/TwitterEmbed.js",
             url: "/challenge/twitter-embed"
         },
         {
             title: "Holadok",
-            desc: "Voluptate Lorem aliqua fugiat fugiat commodo.",
+            desc: "The Holadok Hero component is a visually striking and highly functional element of a doctor appointment website. It features a clean and modern design, with a left side that includes a headline, subheadline, and a single call-to-action button inviting users to make an appointment... - codedesign",
             imageUrl: "/image/preview-holadok.png",
-            sourceUrl: "/challenge/holadok",
+            sourceUrl: "https://github.com/irmankurniawan11/react-challenge/blob/main/src/pages/Holadok.js",
             url: "/challenge/holadok"
         }
     ]
@@ -58,7 +58,7 @@ const Challenges = () => {
             <div className="container max-w-6xl mx-auto px-8 sm:px-0">
                 <div className="flex items-center justify-between">
                     <h1 className="font-bold text-3xl sm:text-4xl mt-4 mb-6">Challenges</h1>
-                    <a href="/challenges" className="font-bold flex items-center">View All<ArrowSmallRightIcon className="w-4 h-4 ml-1"/></a>
+                    <a href="/challenges" className="font-bold flex items-center">View All<ChevronRightIcon className="w-4 h-4 ml-1"/></a>
                 </div>
                 <div className="flex gap-6 flex-wrap">
                     {
