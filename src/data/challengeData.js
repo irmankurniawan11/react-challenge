db88cac3196fe868a23b036e41c0c032a72771a1
irmankurniawan11 <irmankurniawan99@gmail.com>
2023-06-05 09:53:23 +0700

    Add SVG icon

---
+++ src/data/challengeData.js
@@ -0,0 +1,20 @@
+const challengeData = [
+    {
+        title: "Twitter Embed",
+        desc: "Twitter Embed is a simple card that displays a single tweet from Twitter. It includes the user's avatar, name, username, the tweet itself, the total number of likes, and the date it was published... - codedesign",
+        imageUrl: "/image/preview-twitter-embed.png",
+        sourceUrl: "https://github.com/irmankurniawan11/react-challenge/blob/main/src/pages/TwitterEmbed.js",
+        url: "/challenge/twitter-embed",
+        stacks: ["react","tailwind"]
+    },
+    {
+        title: "Holadok",
+        desc: "The Holadok Hero component is a visually striking and highly functional element of a doctor appointment website. It features a clean and modern design, with a left side that includes a headline, subheadline, and a single call-to-action button inviting users to make an appointment... - codedesign",
+        imageUrl: "/image/preview-holadok.png",
+        sourceUrl: "https://github.com/irmankurniawan11/react-challenge/blob/main/src/pages/Holadok.js",
+        url: "/challenge/holadok",
+        stacks: ["react","tailwind"]
+    }
+]
+
+export default challengeData;
---
+++ src/data/projectData.js
@@ -0,0 +1,12 @@
+const projectData = [
+    {
+        title: "Extra Time - Landing Page",
+        desc: "Extra Time is landing page website",
+        imageUrl: "/image/preview-extratime.png",
+        sourceUrl: "#",
+        url: "https://react-football-six.vercel.app/",
+        stacks: ["react","tailwind"]
+    }
+]
+
+export default projectData;
